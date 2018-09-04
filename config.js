ecad7e0b568a3fb5a9ae4b749659965f39673135
raunakhajela <raunakhajela@gmail.com>
2018-09-04 07:01:07 +0000

    setting up twitter bot

---
+++ config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    consumer_key:         'pXHuIFDkD2jVUK9zRVKRjClGD',
+    consumer_secret:      'jq82JBeD7wNJh1bhjLE2ZyBCV3JR8jfNDUPItK8ABQdA3odvrr',
+    access_token:         '3370388173-gKrmNVewDiETMGs0Xs4R5Z7r2jUZKKDuEVswhds',
+    access_token_secret:  'qy9IBAFKOer94L6JUSGgZipT2aK8FeNrdWyAjhRxakP00',
+    timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
+    strictSSL:            true,     // optional - requires SSL certificates to be valid.
+}
