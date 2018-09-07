4cdc5bc8408a8bf72ccb46f2b71443249b33f46d
raunakhajela <raunakhajela@gmail.com>
2018-09-07 12:25:10 +0000

    added setInterval which posts tweet every 20sec

--- bot.js
+++ bot.js
@@ -5,16 +5,24 @@ var Twit = require('twit')
 var config = require('./config');
 var T = new Twit(config);
 
-var tweet = {
-    status: 'Hello from #raunakhajela twitter bot',
-};
+tweetIt();
+setInterval(tweetIt, 1000*20);
 
-T.post('statuses/update', tweet, tweeted);
+function tweetIt(){
 
-function tweeted(err, data, response){
-    if(err){
-        console.log("Something went wrong!");
-    }else{
-        console.log("It worked!");
+    var r = Math.floor(Math.random()*100);
+
+    var tweet = {
+        status: 'Here is a random number ' + r + ' #raunakhajela from twitter bot'
+    };
+
+    T.post('statuses/update', tweet, tweeted);
+
+    function tweeted(err, data, response){
+        if(err){
+            console.log("Something went wrong!");
+        }else{
+            console.log("It worked!");
+        }
     }
 }
